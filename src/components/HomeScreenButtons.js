6dad79cf924057769668334253d27c289f674d7e
wley3337 <wley3337@gmail.com>
2018-08-30 07:49:13 -0400

    organized routes changed login container to homescreenbuttons

--- src/components/HomeScreenButtons.js
+++ src/components/HomeScreenButtons.js
@@ -1,17 +1,16 @@
 import { Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-
-
 import React from 'react'
 
-const LoginContainer = ({match}) =>
+const HomeScreenButtons = ({match}) =>
     <div>
-        LoginContainer
+        HomeScreenButtons
+
        
         <Button inverted as={ Link }  to={`${match.url}login`} color="blue">Log In</Button>
         <Button inverted as={ Link }  to={`${match.url}create-user`} color="teal">Create Account</Button>
-        
+       
         
     </div>
 
-export default LoginContainer
+export default HomeScreenButtons
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux'
 // import * as actions from '../redux/actions'
-import LoginContainer from './LoginContainer';
-import ViewContainer from './ViewContainer';
-import SelectionContainer from './SelectionContainer'
 import LoginForm from '../components/LoginForm'
 import CreateUserForm from '../components/CreateUserForm'
+import HomeScreenButtons from '../components/HomeScreenButtons';
+import ViewContainer from './ViewContainer';
+import SelectionContainer from './SelectionContainer'
+
 import { Route } from 'react-router-dom'
 
 class App extends Component {
@@ -14,11 +15,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={LoginContainer}/>
-        <Route exact path={`/login`} component={  LoginForm } />
-        <Route exact path={`/create-user`} component={  CreateUserForm } />
-        <Route exact path="/my-page" component={ViewContainer}/>
-        <Route exact path="/my-page" component={SelectionContainer}/>
+          <Route exact path="/" component={HomeScreenButtons}/>
+          <Route exact path="/login" component={  LoginForm } />
+          <Route exact path="/create-user" component={  CreateUserForm } />
+          <Route exact path="/my-page" component={ViewContainer}/>
+          <Route exact path="/my-page" component={SelectionContainer}/>
       </div>
     );
   }
