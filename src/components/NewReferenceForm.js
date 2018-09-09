7a9186acd8883a580894bd2c1351135cf02deb7e
wley3337 <wley3337@gmail.com>
2018-09-09 10:25:42 -0400

    added front end error handling of reference not having a formated url and or having blank fields for title and link

--- src/components/NewReferenceForm.js
+++ src/components/NewReferenceForm.js
@@ -8,39 +8,57 @@ import { Redirect } from 'react-router'
 
 
 class NewReferenceForm extends React.Component{
+    state ={
+        error: {title: null, link: null}
+    }
 
 handleCreateNewReference = () =>{
     const {title, link, dropDownValueArray} = this.props.newReference
-    const referenceObj = {
-        title: title,
-        link: link,
-        categoryIdArray: dropDownValueArray
+    if (link.slice(0,5) === "https" && title ){
+        const referenceObj = {
+            title: title,
+            link: link,
+            categoryIdArray: dropDownValueArray
+        }
+        this.props.createNewReference(referenceObj)
+        this.setState({error: {title: null, link: null}})
+    }else{
+        // set local state error to say all urls must beging with https, must have titles that can not be blank
+        this.setState({
+            error: {
+                title: title ? null: "Title can not be blank"
+                , link: link.slice(0,5) === "https" ? null : (link ? "Urls must start with https" : "Link can not be blank")
+            }
+        })
+        
     }
-    debugger
-    this.props.createNewReference(referenceObj)
+    
 }
 
 
 render(){
     return(
       <div id="select-reference-create-input-container">
+          {this.state.error.title ? <p id="select-reference-create-title-error">{this.state.error.title}</p> : null}  
           <input 
             type="text" 
             placeholder="Title of Reference"
             value={this.props.newReference.title} 
             onChange={(e) => this.props.setReferenceTitle(e.target.value)}
           />
+          {this.state.error.link ? <p id="select-reference-create-link-error">{this.state.error.link}</p> : null} 
            <input 
-            type="text" 
-            placeholder="Url Link of Reference"
+            type="url" 
+            placeholder="Url Link to Reference"
             value={this.props.newReference.link} 
             onChange={(e) => this.props.setReferenceLink(e.target.value)}
+           
           />
 
           <button 
             onClick={this.handleCreateNewReference}
           >
-            Create New Reference
+            Create and Add your Reference
           </button>
       </div>
     )
